99bedb2465a9ef99ddfe377e6f0c6d0febd88235
Giovanni Beri <giovanni.beri@epidemicsound.com>
2026-03-28 22:36:49 +0100

    xcodeproj

---
+++ jwm/jwmApp.swift
@@ -0,0 +1,17 @@
+//
+//  jwmApp.swift
+//  jwm
+//
+//  Created by Giovanni Beri on 2026-03-28.
+//
+
+import SwiftUI
+
+@main
+struct jwmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jwmTests/jwmTests.swift
@@ -0,0 +1,17 @@
+//
+//  jwmTests.swift
+//  jwmTests
+//
+//  Created by Giovanni Beri on 2026-03-28.
+//
+
+import Testing
+@testable import jwm
+
+struct jwmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
